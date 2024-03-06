7b5b9753203ec6fbad6ed32faf4723b66591d2c2
alfonsfoubert <alfonsfoubert@gmail.com>
2024-03-06 09:20:03 +0100

    + colorscheme catppuccin + starship configuration

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,17 +1,17 @@
 -- Highlight, edit, and navigate code
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-        -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
 
-        ---@diagnostic disable-next-line: missing-fields
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = { "bash", "c", "html", "lua", "golang", "markdown", "vim", "vimdoc" },
-            -- Autoinstall languages that are not installed
-            auto_install = true,
-            highlight = { enable = true },
-            indent = { enable = true },
-        })
-    end,
+		---@diagnostic disable-next-line: missing-fields
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = { "bash", "c", "html", "lua", "go", "markdown", "vim", "vimdoc" },
+			-- Autoinstall languages that are not installed
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
