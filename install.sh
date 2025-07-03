a1beeee4f8ed9fdc478d4166c980490b0f650c8c
alfonsfoubert <alfons.foubert@gmail.com>
2025-07-03 17:18:06 +0200

    fixed nix packages error

--- install.sh
+++ install.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 export XDG_CONFIG_HOME="$HOME/.config"
 mkdir -p "$XDG_CONFIG_HOME"
+mkdir -p "$XDG_CONFIG_HOME"/nixpkgs
 
-ln -sf "$PWD/ohmyposh" "$XDG_CONFIG_HOME"
 ln -sf "$PWD/nvim" "$XDG_CONFIG_HOME"/nvim
+ln -sf "$PWD/ohmyposh" "$XDG_CONFIG_HOME"/ohmyposh
 
 ln -sf "$PWD/.bash_profile" "$HOME"/.bash_profile
 ln -sf "$PWD/.bashrc" "$HOME"/.bashrc
 ln -sf "$PWD/.tmux.conf" "$HOME"/.tmux.conf
 
-nix-env -iA nixpkgs.myPackages -f config.nix
+ln -sf "$PWD/config.nix" "$XDG_CONFIG_HOME"/nixpkgs/config.nix
+
+nix-env -iA nixpkgs.myPackages
